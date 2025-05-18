a2064faad5c44d0e38c6b2adaf77cfb723adf868
dogglepiyush <131369719+PIYushK0916@users.noreply.github.com>
2025-05-18 14:01:29 +0530

    NEW PUSH

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// tailwind.config.js
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'blue-cashews': ['"Blue Cashews"', 'sans-serif'],
+        'darker-grotesque': ['Darker Grotesque Black', 'sans-serif'],
+      },
+      fontWeight: {
+        'thin': 300,
+        'regular': 400,
+        'medium': 500,
+        // Add more weights as needed
+      },
+    },
+  },
+}
